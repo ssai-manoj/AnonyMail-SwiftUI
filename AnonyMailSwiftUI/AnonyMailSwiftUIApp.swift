c6e5f80f41e141ba2aa1472b47e70057d604adb7
Sai Manoj <smpersonal77@gmail.com>
2023-09-23 14:00:06 +0530

    Initial Commit

---
+++ AnonyMailSwiftUI/AnonyMailSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnonyMailSwiftUIApp.swift
+//  AnonyMailSwiftUI
+//
+//  Created by Sai Manoj Dogiparthi on 23/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AnonyMailSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnonyMailSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnonyMailSwiftUI
+//
+//  Created by Sai Manoj Dogiparthi on 23/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
