e57848b468e1858c12cefca6fe4f7c5fc9f93ebc
Sai Manoj <smpersonal77@gmail.com>
2023-11-17 23:15:34 +0530

    first commit

---
+++ AnonyMailSwiftUI/DropView.swift
@@ -0,0 +1,18 @@
+//
+//  DropView.swift
+//  AnonyMailSwiftUI
+//
+//  Created by Sai Manoj Dogiparthi on 23/09/23.
+//
+
+import SwiftUI
+
+struct DropView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DropView()
+}
